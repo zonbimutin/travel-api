313a06fd6fb996c6b29eed6cfe1e54f05e0bf326
falarcon <zonbimutin@gmail.com>
2020-05-11 16:32:21 +0200

    Token manage Dependencies

--- rest_api/config/bundles.php
+++ rest_api/config/bundles.php
@@ -7,4 +7,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
